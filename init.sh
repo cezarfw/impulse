f8e50ccd8c5ae7b313c777e7277791b7d634b709
cezarfw <cezarfw@gmail.com>
2019-04-02 12:19:54 -0300

    melhoria init.sh e main.yml do ansible

--- init.sh
+++ init.sh
@@ -14,8 +14,30 @@ echo "Atualizando o sistema e instalando repositorio epel"
 echo ""
 yum update -y ; yum install epel-release -y
 
+clear
+
 # Instalar o ansible
 echo "Instalando o ansible, aguarde"
 echo ""
-yum install ansible
+yum install -y ansible
+
+clear
+
+echo "Ansible instalado version: $(ansible --version |sed -n "1p" |awk '{print $2}')"
+echo ""
+
+# Copiando os arquivos do ansible para o respectivo diretorio
+
+echo "Configurando o ansible e alocando os arquivos"
+cp -f $PWD/ansible/hosts /etc/ansible/.
+mkdir /etc/ansible/playbooks
+cp -f $PWD/ansible/main.yml
+
+
+
+
+
+
+
+
 
