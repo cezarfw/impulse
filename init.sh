9843e9600d11f4662332930b6ffe02db4aa8b4cc
Cezar Augusto Roggia <cezarfw@gmail.com>
2019-04-02 13:18:50 -0300

    Melhoria do init.sh e criacao dos diretorios padroes do ansible com as respectivas roles

--- init.sh
+++ init.sh
@@ -7,6 +7,13 @@
 # Data: 29/03/2019
 #
 
+DIR_ROLES_ANSIBLE='/etc/ansible/roles'
+DIR_ANSIBLE='/etc/ansible'
+ROLE_SYSTEM='system_config'
+ROLE_APP='app'
+ROLE_MAIL='mail'
+PLAYBOOKS='/etc/ansible/playbooks'
+
 clear
 
 # Atualizar os pacotes e instalando o repositorio epel
@@ -25,17 +32,28 @@ clear
 
 echo "Ansible instalado version: $(ansible --version |sed -n "1p" |awk '{print $2}')"
 echo ""
+sleep 5
+
+# Entrando no diretorio das roles e criando as mesmas
+echo "Criando as respectivas roles do ansible"
+echo ""
+( cd $DIR_ROLES_ANSIBLE ; ansible-galaxy init $ROLE_SYSTEM ; ansible-galaxy init $ROLE_APP ; ansible-galaxy init $ROLE_MAIL ) 
 
 # Copiando os arquivos do ansible para o respectivo diretorio
 
 echo "Configurando o ansible e alocando os arquivos"
 cp -f $PWD/ansible/hosts /etc/ansible/.
-mkdir /etc/ansible/playbooks
-cp -f $PWD/ansible/main.yml
 
+mkdir -p $PLAYBOOKS
 
+cp -f $PWD/ansible/playbooks/* $PLAYBOOKS/.
 
+echo ""
+echo "Copiando as roles para os respectivos diretorios"
 
+cp -f $PWD/ansible/roles/system_config/tasks/main.yml $DIR_ANSIBLE/$ROLE_SYSTEM/tasks/.
+
+ 
 
 
 
