22d78c70b69daa77d3916af12445beb2d2028a16
cezarfw <cezarfw@gmail.com>
2019-04-02 22:27:39 -0300

    segmentacao das roles e melhoria do init.sh

--- init.sh
+++ init.sh
@@ -9,8 +9,8 @@
 
 DIR_ROLES_ANSIBLE='/etc/ansible/roles'
 DIR_ANSIBLE='/etc/ansible'
-ROLE_SYSTEM='system_config'
 ROLE_APP='app'
+ROLE_WEBSERVER='webserver'
 ROLE_MAIL='mail'
 PLAYBOOKS='/etc/ansible/playbooks'
 
@@ -35,7 +35,7 @@ echo ""
 sleep 5
 
 # Testando se as roles ja estao criadas
-if [ -e $DIR_ROLES_ANSIBLE ]
+if [ -e $DIR_ROLES_ANSIBLE/app ]
 then 
 	echo -e " \033[0;32m As roles ja existem, prosseguindo com a execucao do script \033[0m"
         echo ""
@@ -43,7 +43,8 @@ else
 # Entrando no diretorio das roles e criando as mesmas
 	echo "Criando as respectivas roles do ansible"
 	echo ""
-	( cd $DIR_ROLES_ANSIBLE ; ansible-galaxy init $ROLE_SYSTEM ; ansible-galaxy init $ROLE_APP ; ansible-galaxy init $ROLE_MAIL )fi
+	( cd $DIR_ROLES_ANSIBLE ; ansible-galaxy init $ROLE_APP ; ansible-galaxy init $ROLE_MAIL ; ansible-galaxy init $ROLE_WEBSERVER )
+fi
 
 # Copiando os arquivos do ansible para o respectivo diretorio
 
@@ -54,9 +55,13 @@ mkdir -p $PLAYBOOKS
 
 cp -f $PWD/ansible/playbooks/* $PLAYBOOKS/.
 
-cp -f $PWD/ansible/roles/system_config/tasks/main.yml $DIR_ROLES_ANSIBLE/$ROLE_SYSTEM/tasks/.
+cp -f $PWD/ansible/roles/app/tasks/main.yml $DIR_ROLES_ANSIBLE/$ROLE_APP/tasks/.
 
 cp -f $PWD/ansible/roles/app/files/* $DIR_ROLES_ANSIBLE/$ROLE_APP/files/. 
+
+cp -f $PWD/ssl/* $DIR_ROLES_ANSIBLE/$ROLE_WEBSERVER/files/.
+
+cp -f $PWD/emailconfig/* $DIR_ROLES_ANSIBLE/$ROLE_MAIL/files/.
 
 clear
 echo -e "\033[0;32m Instalacao finalizada, seu servidor esta pronto para operar \033[0m"
