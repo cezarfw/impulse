b22d5b97232f58217c4a67e9927e82eb5dd11061
Cezar Augusto Roggia <cezarfw@gmail.com>
2019-04-04 14:08:28 -0300

    Melhoria do script de carga

--- scripts/carga.sh
+++ scripts/carga.sh
@@ -16,22 +16,22 @@
 
 
 export CONT_REQUISICOES=10000
-export CONT_USUARIOS=10
+export CONT_USUARIOS=30
 
 
 
 while [ $? != 24 ]
 do
-CONT_REQUISICOES=$(( CONT_REQUISICOES+70000 ))
-CONT_USUARIOS=$(( CONT_USUARIOS+10 ))
+CONT_REQUISICOES=$(( CONT_REQUISICOES+200000 ))
+CONT_USUARIOS=$(( CONT_USUARIOS+20 ))
 export CALC=`expr $CONT_REQUISICOES / $CONT_USUARIOS`
-echo "Testando com $CONT_REQUISICOES requisições e $CONT_USUARIOS usuários, isso dá em média $CALC requisições por usuário!"
-ab -n $CONT_REQUISICOES -c $CONT_USUARIOS -q -H "Accept-Encoding: gzip, deflate" $(hostname -i):3000/opt/app.js
+echo "Testando com $CONT_REQUISICOES requisicoes e $CONT_USUARIOS usuarios, isso da em media $CALC requisicoes por usuario!"
+ab -n $CONT_REQUISICOES -c $CONT_USUARIOS -q -H "Accept-Encoding: gzip, deflate" http://localhost:80/opt/app/app.js
 done
 
-echo "O teste de carga realizado mostrou que a aplicãção respondeu até $CONT_REQUISICOES requisições de $CONT_USUARIOS simultâneos."
+echo "O teste de carga realizado mostrou que a aplicacao respondeu $CONT_REQUISICOES requisicoes de $CONT_USUARIOS simultaneos."
 
 
 export CALC1=`expr $CONT_REQUISICOES / $CONT_USUARIOS`
 
-echo "Isso dá um total de $CONT_REQUISICOES requisições oriundos de $CONT_USUARIOS usuários, são $CALC1 requisições por usuário"
+echo "Isso da um total de $CONT_REQUISICOES requisicoes oriundos de $CONT_USUARIOS usuarios, sao $CALC1 requisicoes por usuario"
